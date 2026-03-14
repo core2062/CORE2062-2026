4efef2dc3a0f16867071d616e92aaa771d7162fc
COREProgrammers <COREProgrammers@users.noreply.github.com>
2026-03-14 16:18:16 -0500

    Removed unneccesary code

--- src/main/java/frc/robot/Commands/AimToHubAuto.java
+++ src/main/java/frc/robot/Commands/AimToHubAuto.java
@@ -1,11 +1,8 @@
 package frc.robot.commands;
 
-import com.ctre.phoenix6.swerve.SwerveRequest;
-
-import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.constants.Constants;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
@@ -21,44 +18,15 @@ public class AimToHubAuto extends Command {
     private final SwerveRequest.RobotCentric driveRequest = new SwerveRequest.RobotCentric();
     private final PIDController anglePID=new PIDController(0.9, 0, 0);
     private final PIDController drivePID=new PIDController(0.4,0,0);
-    private final SlewRateLimiter fowardlimit=new SlewRateLimiter(6.0);
-    private final SlewRateLimiter rotationlimit=new SlewRateLimiter(12.0);
     private double limitedForward=0;
     private double limitedTurn=0;
-    
-    
-    /*private double distanceToHub=0;
-    private double distanceError=0;
-    private final double distanceAprilTagToHub=0.6096; //0.923798 meters for comp 
-    private final double aprilTagHeight=0.7874;
-    private double aprilTagDistance=0;
-    private double aprilTagRotation=0;
-    private double turnAngle=0;
-    private double poseAmbiguity=0;
-    private double limitedForward=0;
-    private double limitedTurn=0;
-    private double distanceToHubXY=0;
-    private double hubX=0;
-    private double hubY=0;
-    private double hubZ=0;
-    
-    private final Transform3d tagToHub=new Transform3d(
-        new Translation3d(-0.6096, 0.0, 0.3048), //X: -0.6096 Y: 0 Z: 0.3048
-        new Rotation3d()
-    );
-    private final Transform3d robotToCamera = new Transform3d(
-    new Translation3d(0.0, 0.0, 0.0), // X, Y, Z in meters
-    new Rotation3d(0, 0, Math.PI/2)  // Rotated 90 degrees (left)
-);
-    private final double targetDistance=3; // in meters*/
-      public AimToHubAuto(CommandSwerveDrivetrain s_Swerve, PhotonVisionSubsystem p_vision, double forward, double rotationOutput) {
+      public AimToHubAuto(CommandSwerveDrivetrain s_Swerve, PhotonVisionSubsystem p_vision, GenericHID controller) {
         this.s_Swerve = s_Swerve;
         this.p_vision = p_vision;
-        this.forward = forward;
-        this.rotationOutput = rotationOutput;
+        this.controller = controller;
         anglePID.enableContinuousInput(-Math.PI, Math.PI);
-        anglePID.setTolerance(Units.degreesToRadians(0.75));
-        drivePID.setTolerance(0.02);
+        anglePID.setTolerance(Units.degreesToRadians(3));
+        drivePID.setTolerance(0.04);
         
         // This tells the robot that this command uses the drivetrain
         addRequirements(p_vision);
@@ -67,55 +35,8 @@ public class AimToHubAuto extends Command {
     @Override
     public void execute(){
        
-        double forward = -this.forward * Constants.Swerve.maxSpeed;
-        double rotationOutput = -this.rotationOutput * Constants.Swerve.maxAngularVelocity;
-
-        /*boolean targetVisible = false;
-        hubX = 0;
-        hubY = 0;
-        turnAngle = 0;
-
-        var results = camera.getAllUnreadResults();
-                if (!results.isEmpty()) {
-            // Camera processed a new frame since last
-            // Get the last one in the list.
-            var result = results.get(results.size() - 1);
-            
-            if (result.hasTargets()) {
-                // At least one AprilTag was seen by the camera
-                for (var target : result.getTargets()) {
-                    
-                    poseAmbiguity = target.getPoseAmbiguity();
-                    if(poseAmbiguity<0.5){
-                    if (target.getFiducialId() == 1) {
-                        targetVisible = true;
-                        var transform = target.getBestCameraToTarget();
-                        Transform3d cameraToTarget = target.getBestCameraToTarget();
-                        Transform3d robotToTarget = robotToCamera.plus(cameraToTarget);
-                        Pose3d robotPose = new Pose3d();
-                        Pose3d hubPose = robotPose.transformBy(robotToCamera)
-                          .transformBy(cameraToTarget)
-                          .transformBy(tagToHub);
-                         hubX=hubPose.getX();
-                         hubY=hubPose.getY();
-                         hubZ=hubPose.getZ();
-                        aprilTagRotation=transform.getRotation().getZ();
-                           if(aprilTagRotation<0){
-                                aprilTagRotation+=Math.PI*2;
-                            }
-                        //aprilTagDistance=Math.sqrt(Math.pow(tagX,2)+Math.pow(tagY, 2)+Math.pow(tagZ, 2));//Distance formula
-
-                        //distanceToHub=Math.sqrt(Math.pow(distanceAprilTagToHub,2)+Math.pow(aprilTagDistance, 2)-2*distanceAprilTagToHub*aprilTagDistance*Math.cos(aprilTagRotation));//Law of cosines
-                        //turnAngle=Math.asin(distanceAprilTagToHub*Math.sin(aprilTagRotation)/distanceToHub);//Law sin
-                        distanceToHubXY=Math.sqrt(Math.pow(hubX, 2)+Math.pow(hubY, 2));
-                        turnAngle=Math.atan2(hubY, hubX)-(Math.PI/2);
-                        
-                        }
-                }
-            }
-            }
-        }*/
-        
+        double forward = -controller.getRawAxis(1) * Constants.Swerve.maxSpeed;
+        double rotationOutput = -controller.getRawAxis(4) * Constants.Swerve.maxAngularVelocity;   
            if (p_vision.hasTarget()==true) {
             double turnAngle=0;
             turnAngle=p_vision.getAngleToHub();
