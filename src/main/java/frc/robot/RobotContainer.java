16e45e37548cab983bf6c1f1779794f69cad3759
COREProgrammers <COREProgrammers@users.noreply.github.com>
2026-03-17 20:27:41 -0500

    Adjusted allignment code

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -114,8 +114,7 @@ public class RobotContainer {
             point.withModuleDirection(new Rotation2d(-driver.getLeftY(), -driver.getLeftX()))
         ));
         hubAligner.whileTrue(new AimToHub(drivetrain,
-                                            pv_PhotonVisionSubsystem,
-                                            driver.getHID()
+                                            pv_PhotonVisionSubsystem
                                             ));
         // Run SysId routines when holding back/start and X/Y.
         // Note that each routine should be run exactly once in a single log.
